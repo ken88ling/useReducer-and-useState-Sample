36050efd35f41206b362fd1906c7c441a57b2e51
Ken Ling <ken88ling@gmail.com>
2020-09-25 22:30:14 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [firstName, setFirstName] = useState("");
+
+  return (
+    <div className="App">
+      <button onClick={() => setCount((c) => c + 1)}>+</button>
+
+      <div>count : {count}</div>
+
+      <input
+        value={firstName}
+        onChange={(e) => {
+          setFirstName(e.target.value);
+        }}
+      />
+    </div>
+  );
+}
